372240703eb2d58a9824f165573ff3fea007ff0b
Jingru Xu <nicoxu1000@gmail.com>
2023-04-13 23:45:24 -0400

    test matchers for number and string

--- js/sum.test.js
+++ js/sum.test.js
@@ -9,3 +9,23 @@ describe("truthy or falsy", () => {
     expect(n).not.toBeTruthy();
   }
 });
+
+describe("numbers", () => {
+  it("two plus two", () => {
+    const value = 2 + 2;
+    expect(value).toBe(4);
+    expect(value).toBeGreaterThanOrEqual(3);
+  });
+
+  it("adding floats", () => {
+    const value = 0.2 + 0.1;
+    expect(value).toBeCloseTo(0.3);
+  });
+});
+
+describe("strings", () => {
+  it("there is no I in team", () => {
+    expect("team").not.toMatch(/I/);
+    // expect("team").toMatch("am");
+  });
+});
