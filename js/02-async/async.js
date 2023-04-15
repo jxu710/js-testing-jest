be5f417dcbba8acc8f0256ec983c5f518d9f67be
Jingru Xu <nicoxu1000@gmail.com>
2023-04-15 14:23:51 -0400

    Add testing for async function

--- js/02-async/async.js
+++ js/02-async/async.js
@@ -2,11 +2,9 @@ const axios = require("axios");
 
 const fetchData = async (id) => {
   const results = await axios.get(
-    `https://jsonplaceholder.typicode.com/todo/${id}`
+    `https://jsonplaceholder.typicode.com/todos/${id}`
   );
 
-  console.log(results);
-  return results;
+  return results.data;
 };
-fetchData(1);
 module.exports = fetchData;
