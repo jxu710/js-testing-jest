26ce429de99fe5df01e2185e1708b9c033144c35
Jingru Xu <nicoxu1000@gmail.com>
2023-04-26 19:40:21 -0400

    Add test for confirm password

--- signup-form/src/App.test.js
+++ signup-form/src/App.test.js
@@ -12,6 +12,11 @@ test("inputs should be initially empty", () => {
   render(<App />);
   // 2) finding the elements
   const emailInputElement = screen.getByRole("textbox");
+  const passwordInputElement = screen.getByLabelText("Password");
+  const confirmPasswordInputElement =
+    screen.getByLabelText(/confirm password/i);
   // 3) Assertion
   expect(emailInputElement.value).toBe("");
+  expect(passwordInputElement.value).toBe("");
+  expect(confirmPasswordInputElement.value).toBe("");
 });
