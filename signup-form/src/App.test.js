72fcf06600a7afecf42ab5e5881eb586619e724c
Jingru Xu <nicoxu1000@gmail.com>
2023-04-26 19:27:35 -0400

    add initial test for form input

--- signup-form/src/App.test.js
+++ signup-form/src/App.test.js
@@ -1,8 +1,17 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+// test("renders learn react link", () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+test("inputs should be initially empty", () => {
+  // 1) Rendering the component we want to test
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  // 2) finding the elements
+  const emailInputElement = screen.getByRole("textbox");
+  // 3) Assertion
+  expect(emailInputElement.value).toBe("");
 });
