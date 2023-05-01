dcb5091b90873b166571965a3b1a9bfcd90aee01
Jingru Xu <nicoxu1000@gmail.com>
2023-04-30 23:46:32 -0400

    Add testing for typing email

--- signup-form/src/App.test.js
+++ signup-form/src/App.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import App from "./App";
 
 // test("renders learn react link", () => {
@@ -19,4 +20,14 @@ test("inputs should be initially empty", () => {
   expect(emailInputElement.value).toBe("");
   expect(passwordInputElement.value).toBe("");
   expect(confirmPasswordInputElement.value).toBe("");
+});
+
+test("should be able to type an email", () => {
+  render(<App />);
+  const emailInputElement = screen.getByRole("textbox", {
+    name: /email/i,
+  });
+
+  userEvent.type(emailInputElement, "selena@gmail.com");
+  expect(emailInputElement.value).toBe("selena@gmail.com");
 });
